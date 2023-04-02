da0ec34ee60196bb765537a8197632d823b4d319
Thomas Walsh <22walsht@gmail.com>
2023-04-02 22:43:58 +0100

    turret turning
    pew-pew-pew-pow-poew--powpowpow

--- bote game/scenes/turret.gd
+++ bote game/scenes/turret.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
-
-
 var reloadFull = 20
 var reloading = 20
 
+var target = Vector2(1, 1)
+var targetingPos = true
+const turnSpeed = 0.2
+
+var relativeTurn = 0
+
 func _ready():
-	pass
+	position = Vector2(2, 2)
 
 func _process(delta):
 	if (reloading < reloadFull):
@@ -14,6 +18,12 @@ func _process(delta):
 	else:
 		reloading = 0
 		fire()
+	
+	# Remember turning can only occur if ship not blocking angle! IMPLEMENT THIS
+	if targetingPos:
+		relativeTurn = (target - position).angle() - rotation
+		if relativeTurn:
+			rotation += abs(relativeTurn)/relativeTurn * delta * turnSpeed
 
 func fire():
 	$"Smoke".emitting = true
